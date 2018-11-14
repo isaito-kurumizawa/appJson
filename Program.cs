0ec9820aed1d0a68abe3e02883cb9ccbee31b2bc
isaito-kurumizawa <ikuosaito1989@gmail.com>
2018-11-14 16:02:57 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+// install
+// dotnet add package Microsoft.Extensions.Configuration --version 2.1.1
+// dotnet add package Microsoft.Extensions.Configuration.FileExtensions --version 2.1.1
+// dotnet add package Microsoft.Extensions.Configuration.Json --version 2.1.1
+namespace appJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configBuilder = new ConfigurationBuilder();
+            configBuilder.SetBasePath(Directory.GetCurrentDirectory());
+            configBuilder.AddJsonFile(@"appsettings.json");
+            var config = configBuilder.Build();
+
+            var testValue = config["AppSettings:TestConstValue"];
+            testValue = config.GetSection("AppSettings")["TestConstValue"];
+            var hw = config.GetSection("HelloWorld");
+            Console.WriteLine(hw.Value);
+        }
+    }
+}
